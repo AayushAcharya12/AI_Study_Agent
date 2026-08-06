c5bbbd5e098228ca7f563781fd594bdf5f8ad7cb
Aayush Acharya <aayushacharya373@gmail.com>
2026-08-06 20:58:13 +0545

    Added Agents

--- src/rag.py
+++ src/rag.py
@@ -8,6 +8,7 @@ import pypdf
 from pypdf import PdfReader
 from pathlib import Path
 import docx2txt
+import os
 from tools_used.rrf_fusion import rrf_fusion
 
 #Creating 2 new directories for storing uplodaded files and embeddings
@@ -43,17 +44,11 @@ vectorstore=Chroma(embedding_function=embedding_model,
                    persist_directory="Chroma_db",
                    collection_name="ai_study_docs")
 
-#Lets make our reteriver
-def get_retriever():
-    semantic_reteriver=vectorstore.as_retriever(search_type='similarity',search_kwargs={"k": 10})
-    return semantic_reteriver
-
 
 #Store all the chunks to new file
-allchunks=[]
+all_chunks=[]
 def document_loader(file_path: str) -> int:
     """Read, split, embed, and add a document to the vectorstore.
- 
     Returns the number of chunks that were added.
     """
     text = _extract_text(file_path)
@@ -62,19 +57,45 @@ def document_loader(file_path: str) -> int:
  
     chunks = splitter.split_documents([Document(page_content=text, metadata={"source": Path(file_path).name})])
     vectorstore.add_documents(chunks)
-    allchunks.extend(chunks)
+    all_chunks.extend(chunks)
     return len(chunks)
 
 
+
+#Lets make our reteriver
+def get_retriever():
+    semantic_reteriver=vectorstore.as_retriever(search_type='similarity',search_kwargs={"k": 10})
+    return semantic_reteriver
+
+
+#BM25 
 from langchain_community.retrievers import BM25Retriever
-bm25_retriever = BM25Retriever.from_documents(all_chunks)
-bm25_retriever.k = 10
+def get_bm25(all_chunks):
+    bm25_retriever = BM25Retriever.from_documents(all_chunks)
+    bm25_retriever.k = 10
+    return bm25_retriever
+
+#Cohere Reranker
+import cohere
+co = cohere.Client(os.getenv("COHERE_API_KEY"))
+def cohere_reranker(query,fused_docs):
+    #using reranker
+    documents=[doc.page_content for doc in fused_docs]
+    response = co.rerank(model="rerank-v3.5",query=query,documents=documents,top_n=5)
+    reranked_docs = [fused_docs[result.index] for result in response.results]
+    return reranked_docs
 
 
-#Reteriving from both reteriver(bm25 and semanit)
-def combine_reteriver(query:str)->str:
-    bm25_docs = bm25_retriever.invoke(query)
-    semantic_docs = semantic_retriever.invoke(query)
-    fused_docs=rrf_fusion(bm25_docs,semantic_docs)
+def fusion(all_chunks,query):
+    bm25=get_bm25(all_chunks=all_chunks)
+    semantic=get_retriever()
+    bm25_docs = bm25.invoke(query)
+    semantic_docs = semantic.invoke(query)
+    fused_docs=rrf_fusion([bm25_docs,semantic_docs])
     return fused_docs
 
+
+ #Reteriving from both reteriver(bm25 and semanit) 
+def combine_retriever(fused_docs,query): 
+    reranked_docs=cohere_reranker(query,fused_docs)
+    return reranked_docs
